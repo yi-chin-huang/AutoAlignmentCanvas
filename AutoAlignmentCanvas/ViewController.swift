7a984f8fd8aeffde7e08752c3dee3414dde4b061
Yi-Chin Huang <Yi-Chin@Yi-Chins-MacBook-Pro.local>
2020-10-15 14:18:38 +0800

    clean

--- AutoAlignmentCanvas/ViewController.swift
+++ AutoAlignmentCanvas/ViewController.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class ViewController: UIViewController, UICollisionBehaviorDelegate {
+class ViewController: UIViewController {
 
     private let autoAlignDistance: CGFloat = 4
     private var auxilaryLines: [UIView] = []
